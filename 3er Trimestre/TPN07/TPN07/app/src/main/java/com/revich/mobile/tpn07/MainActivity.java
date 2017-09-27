8fa581c16fc26610a77212928e0aae7f63498536
igalrevich <igalrevich@gmail.com>
2017-09-27 16:50:57 -0300

    GuardaFotoYBuscaGaleria

--- 3er Trimestre/TPN07/TPN07/app/src/main/java/com/revich/mobile/tpn07/MainActivity.java
+++ 3er Trimestre/TPN07/TPN07/app/src/main/java/com/revich/mobile/tpn07/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -15,10 +17,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -27,7 +32,7 @@ import static android.os.Environment.getExternalStoragePublicDirectory;
 public class MainActivity extends AppCompatActivity {
     Button btnSacarFoto, btnSeleccionarFoto, btnVerEstadisticas;
     String mCurrentPhotoPath;
-    int PermisoEscribirExternalStorage;
+    ImageView imgFotoGaleria;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         btnSacarFoto = (Button) findViewById(R.id.btnSacarFoto);
         btnSeleccionarFoto = (Button) findViewById(R.id.btnSeleccionarFoto);
         btnVerEstadisticas = (Button) findViewById(R.id.btnVerEstadisticas);
+        imgFotoGaleria = (ImageView) findViewById(R.id.imgFotoGaleria);
     }
 
     private void SetearListeners() {
@@ -51,7 +57,8 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener btnSacarFoto_click = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-          IntentSacarFoto();
+
+            IntentSacarFoto();
         }
 
     };
@@ -59,6 +66,19 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener btnSeleccionarFoto_click = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+
+            int PICK_IMAGE=2;
+
+            Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            getIntent.setType("image/*");
+
+            Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            pickIntent.setType("image/*");
+
+            Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+            startActivityForResult(chooserIntent, PICK_IMAGE);
 
         }
     };
@@ -74,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -92,22 +112,18 @@ public class MainActivity extends AppCompatActivity {
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-                {
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE)==false)
-                    {
-
-                        ActivityCompat.requestPermissions(this,
-                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                PermisoEscribirExternalStorage);
-
-                    }
-                }
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.TITLE,"FotoTP7.jpg");
+            Uri ContentUri= getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,ContentUri);
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            /*File photoFile = null;
+            try
+            {
                 photoFile = CrearArchivoDeImagen();
-            } catch (IOException ex) {
+            }
+            catch (IOException ex)
+            {
 
                 ex.printStackTrace();
                 // Error occurred while creating the File
@@ -120,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
+            }*/
         }
     }
 
@@ -133,9 +149,11 @@ public class MainActivity extends AppCompatActivity {
         {
             switch (requestCode) {
                 case 1:
-                    galleryAddPic();
                     msg = Toast.makeText(getApplicationContext(), "La foto se saco con exito", Toast.LENGTH_SHORT);
                     msg.show();
+                    break;
+                case 2:
+                    PonerFotoEnImageView(data);
                     break;
             }
         }
@@ -164,6 +182,25 @@ public class MainActivity extends AppCompatActivity {
         values.put(MediaStore.MediaColumns.DATA, filePath);
 
         context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+    }
+
+    private void PonerFotoEnImageView(Intent data)
+    {
+        try
+        {
+            InputStream stream = getContentResolver().openInputStream(data.getData());
+            Bitmap bitmap = BitmapFactory.decodeStream(stream);
+            stream.close();
+            imgFotoGaleria.setImageBitmap(bitmap);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
 }
